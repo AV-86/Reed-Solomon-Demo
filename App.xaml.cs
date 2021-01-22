7e42f885327c7e7afbc82748f50ea071965b5c43
Артем Ворохобин <AV_86@outlook.com>
2021-01-22 11:25:30 +0300

    v1.0Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Reed_Solomon_Demo {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
